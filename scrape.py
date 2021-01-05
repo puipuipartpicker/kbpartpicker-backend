06c9833e96007350382d53604e9f37c005bd3fc6
vimiomori <vivian.muchen@gmail.com>
2021-01-05 19:53:02 +0900

    extract driver config to config file

--- scrape.py
+++ scrape.py
@@ -1,11 +1,8 @@
 import os
-from selenium import webdriver 
-from selenium.webdriver.support.ui import Select
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 
 from vendors import nk_vendor
 from config.database import session
+from config.driver import driver
 
 
 def main(session, driver):
@@ -13,18 +10,6 @@ def main(session, driver):
         nk_vendor.scraper(session, driver, product, nk_vendor.name, nk_vendor.url).run()
 
 if __name__ == "__main__":
-    mode = os.environ.get('MODE')
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("--headless")
-    if mode == 'prd':
-        print('prd')
-        chrome_options.add_argument("--disable-dev-shm-usage")
-        chrome_options.add_argument("--no-sandbox")
-        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-        driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
-    else:
-        print('dev')
-        driver = webdriver.Chrome(options=chrome_options)
     main(session, driver)
     driver.close()
     session.close()
