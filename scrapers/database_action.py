5738901ccdd51b2d10bab34c858fc25a2f1f400e
vimiomori <vivian.muchen@gmail.com>
2021-01-09 14:05:07 +0900

    don't pass product and vendor details at class initiation

--- scrapers/database_action.py
+++ scrapers/database_action.py
@@ -5,21 +5,19 @@ from app.models import Product, Vendor, VendorProductAssociation
 
 class DatabaseAction():
 
-    def __init__(self, session, product, vendor):
+    def __init__(self, session):
         self.session = session
-        self.product = product
-        self.vendor = vendor
 
     def update_or_insert(self, product_details, pv_details):
         product, p_is_new = Product.get_or_create(
             self.session,
             name=product_details.get('name'),
-            product_type=self.product.type
+            product_type=product_details.get('product_type')
         )
         pv, pv_is_new = VendorProductAssociation.get_or_create(
             self.session,
             product_id=product.id,
-            vendor_id=self.vendor.id
+            vendor_id=pv_details.get('vendor')
         )
         # TODO: anyway to do these lines programmatically?
         product.img_url = product_details.get('img_url')
@@ -30,5 +28,6 @@ class DatabaseAction():
         pv.price = pv_details.get('price')
         pv.in_stock = pv_details.get('in_stock')
         pv.url = pv_details.get('pv_url')
+        print(product.hotswap, product.layout, pv.url)
         self.session.commit()
-        print(product.name, pv.vendor.name, "insert" if p_is_new else "update")
+        # print(product.name, pv.vendor.name, "insert" if p_is_new else "update")
