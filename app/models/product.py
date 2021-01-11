483a4887a7824140f75e3aa726db6703926928c3
vimiomori <vivian.muchen@gmail.com>
2021-01-11 23:27:00 +0900

    rename columns for consistency

--- app/models/product.py
+++ app/models/product.py
@@ -11,8 +11,8 @@ class Product(BaseModel):
     name = db.Column(db.String(), nullable=False)
     img_url = db.Column(db.String())
     product_type = db.Column(pgEnum(ProductType, name='product_type'))
-    size = db.Column(pgEnum(SizeType, name='size_type'))
-    layout = db.Column(pgEnum(LayoutType, name='layout_type'))
+    size_type = db.Column(pgEnum(SizeType, name='size_type'))
+    layout_type = db.Column(pgEnum(LayoutType, name='layout_type'))
     stabilizer_type = db.Column(pgEnum(StabilizerType, name='stabilizer_type'))
     hotswap = db.Column(db.Boolean)
 
