a0567b0c0e1e85887c375b902b1292c1fdbed5d9
vimiomori <vivian.muchen@gmail.com>
2021-02-20 21:47:55 +0900

    add debug flag to flask app

--- run.py
+++ run.py
@@ -3,7 +3,10 @@ from api.routes import categories, products
 from api.exception_handlers import *
 from werkzeug.exceptions import HTTPException
 from api.exceptions import AppException, InternalServerError, InvalidUsage, NotFound
+from utils.converters import IntListConverter
 
+
+app.url_map.converters['int_list'] = IntListConverter
 
 app.register_error_handler(HTTPException, handle_http_error)
 app.register_error_handler(Exception, handle_app_error)
@@ -14,4 +17,4 @@ app.register_blueprint(products, url_prefix='/products')
 
 if __name__ == "__main__":
     # app.run(debug=True)
-    app.run(threaded=True, port=5000)
+    app.run(debug=True, threaded=True, port=5000)
