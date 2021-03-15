d880f62a65573a55f9b588a412247a3d29d8c57a
vimiomori <vivian.muchen@gmail.com>
2021-03-15 20:32:24 +0900

    dry up routes

--- api/routes/categories.py
+++ api/routes/categories.py
@@ -5,46 +5,5 @@ from models.types import ProductType
 from api.handlers import search
 
 categories = Blueprint('categories', __name__)
-# CORS(categories, support_credentials=True, resources={r"/*": {"origins": "*"}})
-CORS(categories, resources={r"/*"}, origins=["*"], support_credentials=True)
 
-@categories.route('/switch/search')
-# @cross_origin(supports_credentials=True)
-def switch():
-    product = Product.query.filter_by(product_type=ProductType.switch)
-    return search(product)
-
-
-@categories.route('/pcb/search')
-# @cross_origin(supports_credentials=True)
-def pcb():
-    product = Product.query.filter_by(product_type=ProductType.pcb)
-    return search(product)
-
-
-@categories.route('/case/search')
-# @cross_origin(supports_credentials=True)
-def case():
-    product = Product.query.filter_by(product_type=ProductType.case)
-    return search(product)
-
-
-@categories.route('/plate/search')
-# @cross_origin(supports_credentials=True)
-def plate():
-    product = Product.query.filter_by(product_type=ProductType.plate)
-    return search(product)
-
-
-@categories.route('/stabilizer/search')
-# @cross_origin(supports_credentials=True)
-def stabilizer():
-    product = Product.query.filter_by(product_type=ProductType.stabilizer)
-    return search(product)
-
-
-@categories.route('/keycaps/search')
-# @cross_origin(supports_credentials=True)
-def keycaps():
-    product = Product.query.filter_by(product_type=ProductType.keyset)
-    return search(product)
+categories.add_url_rule('/<product_type>/search', 'search', search)
--- api/routes/products.py
+++ api/routes/products.py
@@ -4,6 +4,6 @@ from api.handlers import get
 
 products = Blueprint('products', __name__)
 # CORS(products, support_credentials=True, resources={r"/*": {"origins": "*"}})
-CORS(products, resources={r"/*"}, origins=["*"], support_credentials=True)
+# CORS(products, resources={r"/*"}, origins=["*"], support_credentials=True)
 
 products.add_url_rule('/<int_list:product_ids>', 'get', get)
