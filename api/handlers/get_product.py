d0b12f5bc3abaa071cb2f3b8e99914c9f50a6af5
vimiomori <vivian.muchen@gmail.com>
2021-03-16 21:42:30 +0900

    use url to find the product record

--- api/handlers/get_product.py
+++ api/handlers/get_product.py
@@ -2,35 +2,31 @@ import re
 import logging
 from flask import request, jsonify
 from vendors import nk_vendor, ck_vendor
-from models import Product
+from models import Product, VendorProductAssociation
 
 logger = logging.getLogger('gunicorn.error')
 
 def get_product():
     vendor_url = request.data.get("vendor_url")
-    product_name = request.data.get("product_name")
-
-    all_vendors = [nk_vendor, ck_vendor]
-    vendor = [v for v in all_vendors if v.config.url in vendor_url][0]
-    logger.info(vendor)
-    if product_config := [
-        p for p in vendor.config.products
-        if f"{vendor.config.url}{p.url}" in vendor_url
-    ]:
-        product_name = re.sub(product_config[0].remove, '', product_name)
+    product_url = request.data.get("product_url")
+    pv_url = _merge_url(vendor_url, product_url)
 
     response = {"status": ""}
-    if product_config and product_name in product_config[0].ignore:
-        logger.info("Not Supported product")
-        response["status"] = "Not Supported"
-    elif product := Product.query.filter(Product.name.ilike(f"%{product_name}%")).first():
-        logger.info("Found product")
-        logger.info(product.to_dict())
-        response["status"] = "Found"
-        response["found_record"] = product.to_dict()
-    else:
-        logger.info("Product Not Found")
+    pv = VendorProductAssociation.query.filter(
+        VendorProductAssociation.url.ilike(f"%{pv_url}")
+    ).first()
+    if not pv:
         response["status"] = "Not Found"
+    else:
+        response["status"] = "Found"
+        response["found_record"] = pv.product.to_dict()
 
     logger.info(response)
     return jsonify(response)
+
+
+def _merge_url(s1, s2):
+    i = 0
+    while not s2.startswith(s1[i:]):
+        i += 1
+    return s1[:i] + s2
