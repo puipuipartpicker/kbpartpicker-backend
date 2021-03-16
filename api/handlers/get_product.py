f46791c2ffca7575e67e775cc15d5e7e5d10afeb
vimiomori <vivian.muchen@gmail.com>
2021-03-16 17:26:20 +0900

    Don't throw error when multiple rows are found

--- api/handlers/get_product.py
+++ api/handlers/get_product.py
@@ -10,9 +10,6 @@ def get_product():
     vendor_url = request.data.get("vendor_url")
     product_name = request.data.get("product_name")
 
-    logger.info(vendor_url)
-    logger.info(product_name)
-
     all_vendors = [nk_vendor, ck_vendor]
     vendor = [v for v in all_vendors if v.config.url in vendor_url][0]
     logger.info(vendor)
@@ -21,13 +18,12 @@ def get_product():
         if f"{vendor.config.url}{p.url}" in vendor_url
     ]:
         product_name = re.sub(product_config[0].remove, '', product_name)
-        logger.info(product_name)
 
     response = {"status": ""}
     if product_config and product_name in product_config[0].ignore:
         logger.info("Not Supported product")
         response["status"] = "Not Supported"
-    elif product := Product.query.filter(Product.name.ilike(f"%{product_name}")).one():
+    elif product := Product.query.filter(Product.name.ilike(f"%{product_name}%")).first():
         logger.info("Found product")
         logger.info(product.to_dict())
         response["status"] = "Found"
