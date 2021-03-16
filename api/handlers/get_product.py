801e7cd1020673da2d6ad59b97c71b66f6c81947
vimiomori <vivian.muchen@gmail.com>
2021-03-16 10:58:10 +0900

    use url to get product type

--- api/handlers/get_product.py
+++ api/handlers/get_product.py
@@ -6,17 +6,22 @@ from models import Product
 def get_product():
     vendor_url = request.data.get("vendor_url")
     product_name = request.data.get("product_name")
-    product_type = request.data.get("product_type")
+
     all_vendors = [nk_vendor, ck_vendor]
     vendor = [v for v in all_vendors if v.config.url in vendor_url][0]
-    product_config = [p for p in vendor.config.products if p.url == product_type][0]
+    if product_config := [
+        p for p in vendor.config.products
+        if f"{vendor.config.url}{p.url}" in vendor_url
+    ]:
+        product_name = re.sub(product_config[0].remove, '', product_name)
+
     response = {"status": ""}
-    database_name = re.sub(product_config.remove, '', product_name)
-    if product_name in product_config.ignore:
+    if product_config and product_name in product_config[0].ignore:
         response["status"] = "Not Supported"
-    elif product := Product.query.filter(Product.name.ilike(f"%{database_name}")).one():
+    elif product := Product.query.filter(Product.name.ilike(f"%{product_name}")).one():
         response["status"] = "Found"
         response["found_record"] = product.to_dict()
     else:
         response["status"] = "Not Found"
+
     return jsonify(response)
