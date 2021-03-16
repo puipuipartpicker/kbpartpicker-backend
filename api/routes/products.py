3afe266b8d1472231a7dcfbeeb0a0ea4c9fa37b2
vimiomori <vivian.muchen@gmail.com>
2021-03-16 11:13:14 +0900

    use POST method for url

--- api/routes/products.py
+++ api/routes/products.py
@@ -4,4 +4,4 @@ from api.handlers import get, get_product
 products = Blueprint('products', __name__)
 
 products.add_url_rule('/<int_list:product_ids>', 'get', get)
-products.add_url_rule('/get_product', 'get_product', get_product)
+products.add_url_rule('/get_product', 'get_product', get_product, methods=['POST'])
