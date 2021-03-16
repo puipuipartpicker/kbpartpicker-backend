923a5741b6b6ee86bf8bfb18e0c6efcf2e0ce6fc
vimiomori <vivian.muchen@gmail.com>
2021-03-16 16:50:49 +0900

    add extensive logging

--- api/handlers/get_product.py
+++ api/handlers/get_product.py
@@ -15,19 +15,26 @@ def get_product():
 
     all_vendors = [nk_vendor, ck_vendor]
     vendor = [v for v in all_vendors if v.config.url in vendor_url][0]
+    logger.info(vendor)
     if product_config := [
         p for p in vendor.config.products
         if f"{vendor.config.url}{p.url}" in vendor_url
     ]:
         product_name = re.sub(product_config[0].remove, '', product_name)
+        logger.info(product_name)
 
     response = {"status": ""}
     if product_config and product_name in product_config[0].ignore:
+        logger.info("Not Supported product")
         response["status"] = "Not Supported"
     elif product := Product.query.filter(Product.name.ilike(f"%{product_name}")).one():
+        logger.info("Found product")
+        logger.info(product.to_dict())
         response["status"] = "Found"
         response["found_record"] = product.to_dict()
     else:
+        logger.info("Product Not Found")
         response["status"] = "Not Found"
 
+    logger.info(response)
     return jsonify(response)
